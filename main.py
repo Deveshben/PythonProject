5929f861b48a2bf72cfb829abcb24977fbbe6587
tusha0508 <113037223+tusha0508@users.noreply.github.com>
2022-09-07 19:56:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello")
+import re
+# Regular expression for validating the mail
+regex = re.compile(
+    r"([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
+
+# Function to validate the mail using Regex module
+
+
+def isValid(email):
+    if re.fullmatch(regex, email):
+        return email
