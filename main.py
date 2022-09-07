417e8925d07ea7f471c3bd4326ca81fd59437cd1
Deveshben <112961928+Deveshben@users.noreply.github.com>
2022-09-07 20:18:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import re
 # Regular expression for validating the mail
 regex = re.compile(
@@ -9,3 +10,44 @@ regex = re.compile(
 def isValid(email):
     if re.fullmatch(regex, email):
         return email
+
+    
+    def sendEmail():
+    try:
+        listOfFinalEmails = readMails()
+        print("Mail will be send of these emails:", listOfFinalEmails)
+        # Reading message from HTML file
+        HTMLFile = open("index.html", "r")
+        messageHTML = HTMLFile.read()
+
+        # SMTP Object
+        print("In process...")
+        mailServer = smt.SMTP("smtp.gmail.com", 587)
+        mailServer.starttls()  # Starting the server
+
+        # Setting up the email subject, to, from, and message
+        fromEmail = senderMail
+        toEmail = listOfFinalEmails
+        Emailmessage = MIMEMultipart("alternative")
+        Emailmessage['Subject'] = "Testing for Python Project"
+        Emailmessage['from'] = senderMail
+
+        # Login via email
+        mailServer.login(senderMail, senderPass)
+        print("Login Successful")
+
+        # Attach the message and Multipart
+        textMsg = MIMEText(messageHTML, "html")
+        Emailmessage.attach(textMsg)
+
+        # Sending the email
+        mailServer.sendmail(fromEmail, toEmail, Emailmessage.as_string())
+        mailServer.quit()
+        print("sent")
+        if(mailServer.send_message):
+            return True
+        else:
+            return False
+
+    except Exception as error:
+        print(error)
